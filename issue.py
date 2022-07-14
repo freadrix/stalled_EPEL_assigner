ecda3d2ed767bef2b39ce919ab2d8cee7742ff18
Anton Medvedev <amedvede@redhat.com>
2022-07-14 13:47:05 +0200

    feat: analyze of fas or sig name

--- issue.py
+++ issue.py
@@ -5,19 +5,45 @@ class Issue(object):
     def __init__(self, issue_title, requester_fas_name, issue_text, issue_url):
         self.issue_text = issue_text
         self.issue_url = issue_url
-        self.is_requester_who_opened_issue = self._analyze_issue_text()
         self.issue_title = self._skip_unnecessary_symbols(issue_title)
         self._git_url = None
         self.package_name = None
         self._get_package_name()
-        self.requester_fas_name = requester_fas_name
+        self.requester_fas_name = self._analyze_issue_text(requester_fas_name)
+        self.is_requester_found = False
 
-    def _analyze_issue_text(self):
+    def _analyze_issue_text(self, name_that_opened):
         keywords = ["sig", "fas"]
         self.issue_text = self._skip_unnecessary_symbols(self.issue_text)
-        return not any(keyword in self.issue_text for keyword in keywords)
+        list_of_words_in_text = self.issue_text.split(" ")
+        index = list_of_words_in_text.index("requesting")
+        if "sig" in self.issue_text:
+            sig_name = list_of_words_in_text[index + 1]
+            if "@" in sig_name:
+                sig_name = sig_name.replace("@", "")
+            return sig_name
+        else:
+            fas_name = list_of_words_in_text[index + 1]
+            if len(fas_name) < 5:
+                fas_name = list_of_words_in_text[index + 2]
+                if len(fas_name) < 5:
+                    fas_name = list_of_words_in_text[index + 3]
+            return fas_name
 
-    def _skip_unnecessary_symbols(self, text):
+    @staticmethod
+    def _is_fas_name(name):
+        url = "https://fasjson.fedoraproject.org/v1/users/" + name + "/"
+        response = requests.get(url=url)
+        return response.status_code == 200
+
+    @staticmethod
+    def _is_group_name(name):
+        url = "https://fasjson.fedoraproject.org/v1/groups/" + name + "/"
+        response = requests.get(url=url)
+        return response.status_code == 200
+
+    @staticmethod
+    def _skip_unnecessary_symbols(text):
         skips = [".", ", ", ":", ";", "'", '"']
         for ch in skips:
             text = text.replace(ch, "")
@@ -48,10 +74,10 @@ class Issue(object):
 
     def _get_git_url(self, package_name):
         git_url = "https://src.fedoraproject.org/" + package_name + ".git"
-        if self._is_git_url_exist(git_url):
+        if self._is_url_valid(git_url):
             self._git_url = git_url
 
-    def _is_git_url_exist(self, git_url):
+    def _is_url_valid(self, git_url):
         response = requests.get(git_url)
         if response.status_code == 200:
             return True
--- runner.py
+++ runner.py
@@ -1,13 +1,20 @@
 from stalled_epel_package_fetcher import StalledEpelPackageFetcher
+import requests
+import os
 
 
 def main():
-    fetcher = StalledEpelPackageFetcher()
-    stalled_epel_issues = fetcher.get_issues()
-    for issue in stalled_epel_issues:
-        print(issue.package_name)
-        print(issue.is_requester_who_opened_issue)
-        print(issue.issue_url)
+    # os.system("fkinit -u amedvede")
+    response = requests.get(url="https://fasjson.fedoraproject.org/v1/groups/epel-packagers-sig/")
+    print(response.status_code, response.json())
+    # fetcher = StalledEpelPackageFetcher()
+    # stalled_epel_issues = fetcher.get_issues()
+    # for issue in stalled_epel_issues:
+    #     print(issue.package_name)
+    #     print(issue.issue_url)
+    #     print(issue.requester_fas_name)
+    #     # print(issue.is_requester_found)
+    #     print("\n")
 
 
 if __name__ == "__main__":
