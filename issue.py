91dcd9f8cd07c3490a7d86cfa6f59c3ba22ff41c
Anton Medvedev <amedvede@redhat.com>
2022-08-01 19:57:10 +0200

    feat: finished issue analyze

--- issue.py
+++ issue.py
@@ -4,35 +4,35 @@ import requests
 from requests_kerberos import HTTPKerberosAuth
 
 
+def make_text_lower(text):
+    return text.lower()
+
+
+def skip_unnecessary_symbols(text):
+    skips = [".", ", ", ":", ";", "'", '"', "@", "`"]
+    for ch in skips:
+        text = text.replace(ch, "")
+    return text
+
+
+def strip_each_element(list_of_words):
+    new_list_of_words = list(map(str.strip, list_of_words))
+    return new_list_of_words
+
+
 class Issue(object):
-    def __init__(self, issue_title, issue_opener, issue_text, issue_url):
-        self.issue_text = self._skip_unnecessary_symbols(self._make_text_lower(issue_text))
+    def __init__(self, issue_title, unprocessed_package_name, issue_opener, issue_text, issue_url):
+        self.issue_text = skip_unnecessary_symbols(make_text_lower(issue_text))
         self.is_requester_found = False
         self.requester_name = self._get_requester_name()
         self.issue_url = issue_url
-        self.issue_title = issue_title
-        self.package_name = self._get_package_name()
+        self._issue_title = issue_title
+        self.package_name = self._get_package_name(unprocessed_package_name)
         self._git_url = None
-
-    @staticmethod
-    def _make_text_lower(text):
-        return text.lower()
-
-    @staticmethod
-    def _skip_unnecessary_symbols(text):
-        skips = [".", ", ", ":", ";", "'", '"', "@", "`"]
-        for ch in skips:
-            text = text.replace(ch, "")
-        return text
-
-    @staticmethod
-    def _strip_each_element(list_of_words):
-        new_list_of_words = list(map(str.strip, list_of_words))
-        return new_list_of_words
 
     def _get_requester_name(self):
         list_of_words_in_text = self.issue_text.split(" ")
-        list_of_words = self._strip_each_element(list_of_words_in_text)
+        list_of_words = strip_each_element(list_of_words_in_text)
         index = list_of_words.index("requesting")
         fas_name_index = self._get_fas_name_index(index, list_of_words)
         if fas_name_index != 0:
@@ -61,12 +61,8 @@ class Issue(object):
         response = requests.get(url=url, auth=HTTPKerberosAuth())
         return response.status_code == 200
 
-    def _get_package_name(self):
+    def _get_package_name(self, unprocessed_package_name):
         package_prefixes = ("rpms/", "flatpaks/", "modules/", "tests/", "container/")
-        if "stalled epel package" in self.issue_title.lower():
-            unprocessed_package_name = self.issue_title[21:].strip()
-        else:
-            unprocessed_package_name = self.issue_title
         if unprocessed_package_name.startswith(package_prefixes):
             self._get_git_url(unprocessed_package_name)
             if self._is_git_url_valid():
--- stalled_epel_package_fetcher.py
+++ stalled_epel_package_fetcher.py
@@ -1,39 +1,94 @@
 import requests
+import re
 from issue import Issue
+from issue import skip_unnecessary_symbols, make_text_lower, strip_each_element
 
 
 class StalledEpelPackageFetcher(object):
     def __init__(self):
+        self._list_of_issue_objs = []
+        self._issue = None
         self._params = {
             "status": "Closed",
             "per_page": 20
         }
-        self._keyword = "stalled epel package:"
+        self._keyword_for_single_issue = "stalled epel package:"
+        self._keyword_for_multiple_issues = "stalled epel packages:"
         self._api_endpoint = "https://pagure.io/api/0/releng/issues"
 
     def get_issues(self):
-        list_of_issues = []
         number_of_pages = self._get_number_of_pages()
         for page in range(5):  # for _ in range(number_of_pages)
             self._params["page"] = page + 1
             response = requests.get(url=self._api_endpoint, params=self._params)
             issues = response.json()["issues"]
-            # print(issues)
             for issue in issues:
-                issue_title = issue["title"]
-                if self._is_contain_keyword(issue_title):
-                    issue_opener = issue["user"]["name"]
-                    issue_url = issue["full_url"]
-                    issue_text = issue["content"].lower()
-                    issue = Issue(issue_title, issue_opener, issue_text, issue_url)
-                    list_of_issues.append(issue)
+                self._issue = issue
+                self._select_type_of_issue()
                 self._params["page"] += 1
-        return list_of_issues
+        return self._list_of_issue_objs
+
+    def _select_type_of_issue(self):
+        issue_title = self._issue["title"]
+        if self._is_contain_keyword_for_single_issue(issue_title):
+            self._process_issue()
+        elif self._is_contain_keyword_for_multiple_issues(issue_title):
+            self._process_issues()
+
+    def _process_issue(self):
+        issue_title = self._issue["title"]
+        issue_opener = self._issue["user"]["name"]
+        issue_url = self._issue["full_url"]
+        issue_text = self._issue["content"].lower()
+        unprocessed_package_name = issue_title[len(self._keyword_for_single_issue):].strip()
+        issue_obj = Issue(issue_title, unprocessed_package_name, issue_opener, issue_text, issue_url)
+        self._list_of_issue_objs.append(issue_obj)
+
+    def _process_issues(self):
+        issue_title = self._issue["title"]
+        issue_opener = self._issue["user"]["name"]
+        issue_url = self._issue["full_url"]
+        issue_text = self._issue["content"]
+        unprocessed_package_names = issue_title[len(self._keyword_for_multiple_issues):].strip()
+        unprocessed_package_names = self._skip_unnecessary_words_between_package_names(unprocessed_package_names)
+        list_of_package_names = unprocessed_package_names.split(" ")
+        list_of_package_names = strip_each_element(list_of_package_names)
+        if list_of_package_names[0].isnumeric():
+            amount_of_packages = int(list_of_package_names[0])
+            package_prefix = list_of_package_names[1]
+            if package_prefix.endswith("s"):
+                package_prefix = package_prefix[:len(package_prefix) - 1]
+            list_of_package_names = self._get_package_names(issue_text, amount_of_packages, package_prefix)
+        for package_name in list_of_package_names:
+            issue_obj = Issue(issue_title, package_name, issue_opener, issue_text, issue_url)
+            self._list_of_issue_objs.append(issue_obj)
+
+    @staticmethod
+    def _get_package_names(text, amount_of_packages, package_prefix):
+        list_of_package_names = []
+        list_of_words_in_text = strip_each_element(re.split(r'" "|\r|\n', skip_unnecessary_symbols(text)))
+        list_of_words_in_text = [el for el in list_of_words_in_text if el]
+        for i in range(len(list_of_words_in_text)):
+            if list_of_words_in_text[i].startswith(package_prefix):
+                for i_offset in range(amount_of_packages):
+                    list_of_package_names.append(list_of_words_in_text[i + i_offset])
+                return list_of_package_names
+        return None
+
+    @staticmethod
+    def _skip_unnecessary_words_between_package_names(package_names):
+        unnecessary_words = ["and"]
+        for word in unnecessary_words:
+            package_names = package_names.replace(word, "")
+        return package_names
 
     def _get_number_of_pages(self):
         response = requests.get(url=self._api_endpoint, params=self._params)
         number_of_pages = response.json()["pagination"]["pages"]
         return number_of_pages
 
-    def _is_contain_keyword(self, issue_title):
-        return self._keyword in issue_title.lower()
+    def _is_contain_keyword_for_single_issue(self, issue_title):
+        return self._keyword_for_single_issue in issue_title.lower()
+
+    def _is_contain_keyword_for_multiple_issues(self, issue_title):
+        return self._keyword_for_multiple_issues in issue_title.lower()
